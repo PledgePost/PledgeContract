d69eb3d96e6901994ef31ecb013a1f1f370eb1ee
shuhei tanaka <67859510+tnkshuuhei@users.noreply.github.com>
2023-11-28 15:11:09 +0900

    made ERC721 upgradable

--- contracts/interface/IPledgePostERC721.sol
+++ contracts/interface/IPledgePostERC721.sol
@@ -1,7 +1,26 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.18;
+pragma solidity ^0.8.19;
 
 interface IPledgePostERC721 {
+    struct TokenData {
+        address minterAddress;
+        address authorAddress;
+        uint256 articleId;
+        string description;
+        string imageUrl;
+    }
+
+    event Minted(
+        address indexed recipient,
+        uint256 tokenId,
+        address indexed author,
+        uint256 indexed articleId,
+        uint256 timestamp
+    );
+    event Burned(address indexed oparator, uint256 indexed tokenId);
+
+    function initialize(address owner, string memory defaultImageUrl) external;
+
     function setImageUrl(uint256 tokenId, string calldata imageUrl) external;
 
     function setDefaultImageUrl(string calldata defaultImageUrl) external;
@@ -15,11 +34,7 @@ interface IPledgePostERC721 {
 
     function tokenURI(uint256 tokenId) external view returns (string memory);
 
-    function burn(uint256[] calldata tokenId) external;
-
     function supportsInterface(bytes4 interfaceId) external view returns (bool);
-
-    function getImageUrl(uint256 tokenId) external view returns (string memory);
 
     function checkOwner(
         address _sender,
