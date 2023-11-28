3c7075bb076b2c83e56ea051730cb4b0776f9342
shuhei tanaka <67859510+tnkshuuhei@users.noreply.github.com>
2023-11-28 17:53:12 +0900

    renamed lib

---
+++ contracts/libraries/Sqrt.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+library Sqrt {
+    // Calculates the Square root of x
+    function sqrt(uint256 x) internal pure returns (uint256 y) {
+        uint256 z = (x + 1) / 2;
+        y = x;
+        while (z < y) {
+            y = z;
+            z = (x / z + z) / 2;
+        }
+    }
+}
