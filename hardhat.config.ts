433a76c545afe203ab6972771d62e7c2859fd7c7
shuhei tanaka <67859510+tnkshuuhei@users.noreply.github.com>
2023-11-29 18:45:26 +0900

    updated packages

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,8 @@
-import "@openzeppelin/hardhat-upgrades";
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
 import * as dotenv from "dotenv";
 dotenv.config();
+import { HardhatUserConfig } from "hardhat/config";
+import "@openzeppelin/hardhat-upgrades";
+import "@nomicfoundation/hardhat-toolbox";
 const providerApiKey = process.env.PROVIDER_API_KEY as string;
 const deployerPrivateKey = process.env.DEPLOYER_PRIVATE_KEY as string;
 const etherscanApiKey = process.env.ETHERSCAN_API_KEY as string;
