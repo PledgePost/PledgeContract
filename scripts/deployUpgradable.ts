534edbe614206f3e251ff9d12fb942da2ffc8ffa
shuhei tanaka <67859510+tnkshuuhei@users.noreply.github.com>
2023-12-07 18:31:44 +0900

    modified deploy scripts

--- scripts/deployUpgradable.ts
+++ scripts/deployUpgradable.ts
@@ -9,6 +9,26 @@ async function main() {
   await contract.waitForDeployment();
 
   console.log("Contract deployed to:", await contract.getAddress());
+  const tx1 = await contract.addAdmin(
+    "0x06aa005386F53Ba7b980c61e0D067CaBc7602a62"
+  );
+  await tx1.wait();
+  console.log(`Admin added: 0x06aa005386F53Ba7b980c61e0D067CaBc7602a62`);
+
+  const tx2 = await contract.createRound(
+    "Initial Round",
+    "This is the first round of the PledgePost! Enjoy to write something awesome!",
+    1699509663,
+    1702101663
+  );
+  await tx2.wait();
+  console.log(`Round created`);
+
+  const tx3 = await contract.postArticle(
+    "bafybeihibat5imkafg6kb27nqseqkyitthfhvlcoruqv7gcqokxlsbdg44"
+  );
+  await tx3.wait();
+  console.log(`Article posted`);
 }
 
 async function upgrade() {
@@ -19,17 +39,9 @@ async function upgrade() {
   );
   console.log("implementation upgraded to:", await contract.getAddress());
 }
-async function initialize() {
-  let contractAddress = "0xD62087Bf50dCd6dD87f96E21d1AD040bD9c99589";
 
-  const contract = await ethers.getContractAt("PledgePost", contractAddress);
-
-  const tx1 = await contract.postArticle(contractAddress);
-  await tx1.wait();
-  console.log(`Article posted`);
-}
 async function addAdmin() {
-  let contractAddress = "0xD62087Bf50dCd6dD87f96E21d1AD040bD9c99589";
+  let contractAddress = "";
 
   const contract = await ethers.getContractAt("PledgePost", contractAddress);
 
@@ -40,10 +52,10 @@ async function addAdmin() {
   console.log(`Admin added`);
 }
 
-// main()
-// initialize()
-// upgrade()
-addAdmin()
+main()
+  // initialize()
+  // upgrade()
+  // addAdmin()
   .then(() => process.exit(0))
   .catch((error) => {
     console.error(error);
@@ -57,4 +69,5 @@ addAdmin()
 	npx hardhat run scripts/deployUpgradable.ts --network polygonMumbai
 	npx hardhat run scripts/deployUpgradable.ts --network polygonZkEvmTestnet
 	npx hardhat run scripts/deployUpgradable.ts --network scrollSepolia
+	npx hardhat verify --network optimismGoerli 0x28b202Ee6a2C7375194F00BAcECDC02c88508025
 */
